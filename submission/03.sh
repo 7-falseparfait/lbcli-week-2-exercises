01d97381fabb3ecc9c0449a81fad0c445ddb9d5f
Osuji <weezdomosuji@gmail.com>
2026-07-01 11:00:45 +0100

    fix: fund SegWit address by mining 101 blocks in 03.sh

--- submission/03.sh
+++ submission/03.sh
@@ -1,4 +1,12 @@
 # Create a SegWit address.
-bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "" bech32
 # Add funds to the address.
 # Return only the Address
+
+# Generate a SegWit address
+ADDR=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "" bech32)
+
+# Fund it by mining 101 blocks to it
+bitcoin-cli -regtest generatetoaddress 101 "$ADDR"
+
+# Return only the address
+echo "$ADDR"
